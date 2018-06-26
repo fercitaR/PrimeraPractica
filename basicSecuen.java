aebec654e1b97cde6be2547719648c10e0446445
fercitaR <mariarojasmafer@gmail.com>
2018-06-26 00:35:58 -0300

    viendo

---
+++ basicSecuen.java
@@ -0,0 +1,28 @@
+package Parte1;
+public class basicSecuen {
+		public static void main() {
+/* 1) Escribe un programa Java que realice lo siguiente: declarar una variable N de tipo int, una variable A de tipo double 
+y una variable C de tipo char y asigna a cada una un valor. A continuación muestra por pantalla:
+El valor de cada variable.
+La suma de N + A
+La diferencia de A - N
+El valor numérico correspondiente al carácter que contiene la variable C.
+Si por ejemplo le hemos dado a N el valor 5, a A el valor 4.56 y a C el valor ‘a’, se debe mostrar por pantalla:
+ */
+			int N = 5;
+			double A = 4.56;
+			char C = 'a';
+			System.out.println("El valor de N es: " + N);
+			System.out.println("El valor de A es: " + A);
+			System.out.println("El valor de C es: " + C);
+			System.out.println("N + A = " + (N+A));
+			System.out.println("A - N = " + (A-N));
+			System.out.println("El valor numerico de C es: " + (int)C);		
+			
+			
+/* 2) 
+ 
+ */		
+ 
+	}
+}
